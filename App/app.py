68f10464023c5be77d7766afb9f2752b0fc3fb69
Mtiwari27 <126952539+Mtiwari27@users.noreply.github.com>
2024-06-18 13:11:29 +0530

    Update app.py

--- App/app.py
+++ App/app.py
@@ -4,10 +4,10 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 MODEL_PATHS = {
-    'XGBoost': "models/xgb_model.pkl",
-    'SVM': "models/model_svm.pkl",
-    'Logistic Regression': "models/model_lr.pkl",
-    'Random Forest': "models/model_rf.pkl"
+    'XGBoost': "Model/model_xgb.pkl",
+    'SVM': "Model/model_svm.pkl",
+    'Logistic Regression': "Model/model_lr.pkl",
+    'Random Forest': "Model/model_rf.pkl"
 }
 
 # Load the selected model
